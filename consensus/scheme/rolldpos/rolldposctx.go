ae3cb61786064510acb36f7bf1f9eba50604d299
lzxm160 <lzxm160@sina.com>
2019-04-25 07:21:07 +0800

    x

--- consensus/scheme/rolldpos/rolldposctx.go
+++ consensus/scheme/rolldpos/rolldposctx.go
@@ -386,7 +386,7 @@ func (ctx *rollDPoSCtx) Commit(msg interface{}) (bool, error) {
 		}
 		// putblock to parent chain if the current node is proposer and current chain is a sub chain
 		if ctx.round.Proposer() == ctx.encodedAddr && ctx.chain.ChainAddress() != "" {
-			putBlockToParentChain(ctx.rootChainAPI, ctx.chain.ChainAddress(), ctx.priKey, ctx.encodedAddr, pendingBlock)
+
 		}
 	} else {
 		ctx.logger().Panic(
