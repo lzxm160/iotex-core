4b9c78544316612f56ac267784a001044ecd270a
lzxm160 <lzxm160@sina.com>
2019-05-01 08:48:42 +0800

    x

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -833,7 +833,7 @@ func TestBlockchainInitialCandidate(t *testing.T) {
 	defer func() {
 		require.NoError(bc.Stop(context.Background()))
 	}()
-	candidate, err := sf.CandidatesByHeight(0)
+	candidate, err := sf.CandidatesByHeight(1)
 	require.Error(err)
 	//require.Equal(24, len(candidate))
 	fmt.Println("c:",candidate)
