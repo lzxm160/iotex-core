429fb4bba8a96d0df006e310bb90dbd184c62910
lzxm160 <lzxm160@sina.com>
2019-05-06 09:13:07 +0800

    x

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/iotexproject/iotex-core/action/protocol/account"
 	accountutil "github.com/iotexproject/iotex-core/action/protocol/account/util"
 	"github.com/iotexproject/iotex-core/action/protocol/execution"
+	"github.com/iotexproject/iotex-core/action/protocol/poll"
 	"github.com/iotexproject/iotex-core/action/protocol/rewarding"
 	"github.com/iotexproject/iotex-core/action/protocol/rolldpos"
 	"github.com/iotexproject/iotex-core/blockchain/block"
