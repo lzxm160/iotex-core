7c5197600f13e117809bbb7acf5fc3fdb20963ce
lzxm160 <lzxm160@sina.com>
2019-05-24 13:45:26 +0800

    x

--- action/protocol/poll/protocol.go
+++ action/protocol/poll/protocol.go
@@ -8,6 +8,7 @@ package poll
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 	"time"
 
@@ -216,6 +217,7 @@ func (p *governanceChainCommitteeProtocol) Validate(ctx context.Context, act act
 }
 
 func (p *governanceChainCommitteeProtocol) delegatesByGravityChainHeight(height uint64) (state.CandidateList, error) {
+	fmt.Println("/////////////////height:", height)
 	r, err := p.electionCommittee.ResultByHeight(height)
 	if err != nil {
 		return nil, err
