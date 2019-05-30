c108b8f164a98d9ae2f63a9809215e3451744500
lzxm160 <lzxm160@sina.com>
2019-05-30 11:21:15 +0800

    x

--- server/itx/subchain_test.go
+++ server/itx/subchain_test.go
@@ -7,7 +7,12 @@
 package itx
 
 import (
+	"context"
 	"testing"
+	"time"
+
+	"github.com/iotexproject/iotex-core/pkg/probe"
+	"github.com/stretchr/testify/require"
 
 	"github.com/iotexproject/iotex-core/config"
 	"github.com/stretchr/testify/assert"
@@ -20,4 +25,28 @@ func TestGetSubChainDBPath(t *testing.T) {
 	trieDBPath := getSubChainDBPath(1, config.Default.Chain.TrieDBPath)
 	assert.Equal(t, "chain-1-chain.db", chainDBPath)
 	assert.Equal(t, "chain-1-trie.db", trieDBPath)
+}
+func TestHandleBlock(t *testing.T) {
+	require := require.New(t)
+	cfg, err := config.New()
+	require.NoError(err)
+	cfg.Consensus.Scheme = config.RollDPoSScheme
+	cfg.Genesis.EnableGravityChainVoting = true
+	ss, err := NewServer(cfg)
+	require.NoError(err)
+	require.NotNil(ss)
+	ctx, cancel := context.WithCancel(context.Background())
+	livenessCtx, livenessCancel := context.WithCancel(context.Background())
+	probeSvr := probe.New(cfg.System.HTTPStatsPort)
+	err = probeSvr.Start(ctx)
+	require.NoError(err)
+	go StartServer(ctx, ss, probeSvr, cfg)
+
+	err = ss.HandleBlock(nil)
+	require.Error(err)
+	time.Sleep(time.Second * 2)
+	cancel()
+	err = probeSvr.Stop(livenessCtx)
+	require.NoError(err)
+	livenessCancel()
 }
