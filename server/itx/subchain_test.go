6424670630583030cf859a8775c6e5c6f8726398
lzxm160 <lzxm160@sina.com>
2019-05-30 13:58:37 +0800

    Revive test coverage #1140

--- server/itx/subchain_test.go
+++ server/itx/subchain_test.go
@@ -7,7 +7,15 @@
 package itx
 
 import (
+	"context"
 	"testing"
+	"time"
+
+	"github.com/iotexproject/go-pkgs/hash"
+	"github.com/iotexproject/iotex-core/blockchain/block"
+	"github.com/iotexproject/iotex-core/pkg/probe"
+	"github.com/iotexproject/iotex-core/test/identityset"
+	"github.com/stretchr/testify/require"
 
 	"github.com/iotexproject/iotex-core/config"
 	"github.com/stretchr/testify/assert"
@@ -20,4 +28,45 @@ func TestGetSubChainDBPath(t *testing.T) {
 	trieDBPath := getSubChainDBPath(1, config.Default.Chain.TrieDBPath)
 	assert.Equal(t, "chain-1-chain.db", chainDBPath)
 	assert.Equal(t, "chain-1-trie.db", trieDBPath)
+}
+func TestHandleBlock(t *testing.T) {
+	require := require.New(t)
+	cfg, err := config.New()
+	require.NoError(err)
+	cfg.Consensus.Scheme = config.RollDPoSScheme
+	cfg.Genesis.EnableGravityChainVoting = true
+	ss, err := NewServer(cfg)
+	require.NoError(err)
+	require.NotNil(ss)
+	ctx, cancel := context.WithCancel(context.Background())
+	livenessCtx, livenessCancel := context.WithCancel(context.Background())
+	probeSvr := probe.New(cfg.System.HTTPStatsPort)
+	err = probeSvr.Start(ctx)
+	require.NoError(err)
+	go StartServer(ctx, ss, probeSvr, cfg)
+	time.Sleep(time.Second * 2)
+
+	rap := block.RunnableActionsBuilder{}
+	ra := rap.
+		SetHeight(1).
+		SetTimeStamp(time.Now()).
+		Build(identityset.PrivateKey(0).PublicKey())
+	blk, err := block.NewBuilder(ra).
+		SetVersion(1).
+		SetReceiptRoot(hash.Hash256b([]byte("hello, world!"))).
+		SetDeltaStateDigest(hash.Hash256b([]byte("world, hello!"))).
+		SetPrevBlockHash(hash.Hash256b([]byte("hello, block!"))).
+		SignAndBuild(identityset.PrivateKey(0))
+	require.NoError(err)
+
+	go func() {
+		err = ss.HandleBlock(&blk)
+		require.NoError(err)
+	}()
+
+	time.Sleep(time.Second * 5)
+	cancel()
+	err = probeSvr.Stop(livenessCtx)
+	require.NoError(err)
+	livenessCancel()
 }
