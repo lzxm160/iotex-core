3576112032a2457ff769f326b7c779dfe1c081af
lzxm160 <lzxm160@sina.com>
2019-05-30 14:34:38 +0800

    Revive test coverage #1140

--- blockchain/genesis/genesis_test.go
+++ blockchain/genesis/genesis_test.go
@@ -38,12 +38,12 @@ func TestHash(t *testing.T) {
 func TestAccount_InitBalances(t *testing.T) {
 	require := require.New(t)
 	InitBalanceMap := make(map[string]string, 0)
-	InitBalanceMap["1"] = "1"
-	InitBalanceMap["2"] = "2"
+	InitBalanceMap["io1emxf8zzqckhgjde6dqd97ts0y3q496gm3fdrl6"] = "1"
+	InitBalanceMap["io1mflp9m6hcgm2qcghchsdqj3z3eccrnekx9p0ms"] = "2"
 	acc := Account{InitBalanceMap}
 	adds, balances := acc.InitBalances()
-	require.Equal("1", adds[0])
-	require.Equal("2", adds[1])
+	require.Equal("io1mflp9m6hcgm2qcghchsdqj3z3eccrnekx9p0ms", adds[0].String())
+	require.Equal("io1mflp9m6hcgm2qcghchsdqj3z3eccrnekx9p0ms", adds[1].String())
 	require.Equal(InitBalanceMap["1"], balances[0].Text(10))
 	require.Equal(InitBalanceMap["2"], balances[1].Text(10))
 }
