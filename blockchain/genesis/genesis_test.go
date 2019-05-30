260f50528d54e819441aea406d02e16ccd256482
lzxm160 <lzxm160@sina.com>
2019-05-30 14:31:53 +0800

    Revive test coverage #1140

--- blockchain/genesis/genesis_test.go
+++ blockchain/genesis/genesis_test.go
@@ -33,5 +33,17 @@ func TestHash(t *testing.T) {
 	cfg, err := New()
 	require.NoError(err)
 	hash := cfg.Hash()
-	require.Equal("", hex.EncodeToString(hash[:]))
+	require.Equal("37d5d692d517428eb4830c2a575ee7e6dad32a46d4a7f39be9463ce3583e02d0", hex.EncodeToString(hash[:]))
+}
+func TestAccount_InitBalances(t *testing.T) {
+	require := require.New(t)
+	InitBalanceMap := make(map[string]string, 0)
+	InitBalanceMap["1"] = "1"
+	InitBalanceMap["2"] = "2"
+	acc := Account{InitBalanceMap}
+	adds, balances := acc.InitBalances()
+	require.Equal("1", adds[0])
+	require.Equal("2", adds[1])
+	require.Equal(InitBalanceMap["1"], balances[0].Text(10))
+	require.Equal(InitBalanceMap["2"], balances[1].Text(10))
 }
