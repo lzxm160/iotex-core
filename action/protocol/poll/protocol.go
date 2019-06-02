ffeb81554b2ca92bc48007144c5c72b2f8f06645
lzxm160 <lzxm160@sina.com>
2019-06-02 08:09:38 +0800

    x

--- action/protocol/poll/protocol.go
+++ action/protocol/poll/protocol.go
@@ -198,7 +198,7 @@ func (p *governanceChainCommitteeProtocol) Initialize(
 	log.L().Info("Initialize poll protocol", zap.Uint64("height", p.initGravityChainHeight))
 	var ds state.CandidateList
 	if ds, err = p.delegatesByGravityChainHeight(p.initGravityChainHeight); err != nil {
-		for _, ok := err.(db.ErrNotExist); ok; {
+		for err.Error()==db.ErrNotExist.Error() {
 			log.L().Error("calling committee,wait for 15 seconds")
 			time.Sleep(time.Second * 15)
 			ds, err = p.delegatesByGravityChainHeight(p.initGravityChainHeight)
