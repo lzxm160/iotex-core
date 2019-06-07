61780dd2cfe2e07dc1c06f54dbcb81cd474b8dcb
lzxm160 <lzxm160@sina.com>
2019-06-07 13:28:57 +0800

    x

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -332,14 +332,12 @@ func (bc *blockchain) ChainAddress() string {
 
 // Start starts the blockchain
 func (bc *blockchain) Start(ctx context.Context) (err error) {
-	log.L().Info("33333333333333333333333")
 	bc.mu.Lock()
 	defer bc.mu.Unlock()
-	log.L().Info("4444444444444444444444444")
 	if err = bc.lifecycle.OnStart(ctx); err != nil {
+		log.L().Panic("what", zap.Error(err))
 		return err
 	}
-	log.L().Info("555555555555555555555555")
 	// get blockchain tip height
 	if bc.tipHeight, err = bc.dao.getBlockchainHeight(); err != nil {
 		return err
