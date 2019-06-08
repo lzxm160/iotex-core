29a6260bae11f831e184b47ee34bcaeaafd01536
lzxm160 <lzxm160@sina.com>
2019-06-08 21:31:04 +0800

    fix comments

--- blocksync/buffer.go
+++ blocksync/buffer.go
@@ -63,7 +63,7 @@ func (b *blockBuffer) Flush(blk *block.Block) (bool, bCheckinResult) {
 		zap.Uint64("requesthei", requestHeight),
 		zap.Uint64("localDbHeight", localDbHeight),
 	)
-	if requestHeight > localDbHeight {
+	if requestHeight >= localDbHeight {
 		return false, bCheckinValid
 	}
 
