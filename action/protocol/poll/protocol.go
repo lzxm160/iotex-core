a5f6a87fd7c9f8bc1a74968ea9a96da7421a9b99
lzxm160 <lzxm160@sina.com>
2019-06-08 08:56:44 +0800

    fix comments

--- action/protocol/poll/protocol.go
+++ action/protocol/poll/protocol.go
@@ -230,6 +230,9 @@ func (p *governanceChainCommitteeProtocol) Validate(ctx context.Context, act act
 }
 
 func (p *governanceChainCommitteeProtocol) delegatesByGravityChainHeight(height uint64) (state.CandidateList, error) {
+	log.L().Info("request hei:", zap.Uint64("requesthei", height))
+	localDbHeight := p.electionCommittee.LatestHeight()
+	log.L().Info("localDbHeight hei:", zap.Uint64("localDbHeight", localDbHeight))
 	r, err := p.electionCommittee.ResultByHeight(height)
 	if err != nil {
 		return nil, err
