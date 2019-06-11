c3ae3a31585bb12b78e056af3f61c4fcb5ae8f1d
lzxm160 <lzxm160@sina.com>
2019-06-11 14:24:47 +0800

    x

--- blocksync/buffer.go
+++ blocksync/buffer.go
@@ -84,6 +84,7 @@ func (b *blockBuffer) Flush(blk *block.Block) (bool, bCheckinResult) {
 	}
 	blkTime, err := getTime(epochHeight)
 	if err != nil {
+		fmt.Println(blkHeight, ":::::::::::", epochNumber, "::::::::::::::", epochHeight, ":::::::::::::::::", blkTime)
 		return false, bCheckinValid
 	}
 
