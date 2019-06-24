6df27e12b83935a0b2c9a0f3f8fa2cda2bcb1f50
lzxm160 <lzxm160@sina.com>
2019-06-25 07:48:25 +0800

    x

--- consensus/scheme/rolldpos/rolldposctx.go
+++ consensus/scheme/rolldpos/rolldposctx.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/facebookgo/clock"
-	"github.com/iotexproject/go-fsm"
 	"github.com/iotexproject/go-pkgs/crypto"
 	"github.com/iotexproject/iotex-address/address"
 	"github.com/pkg/errors"
@@ -473,6 +472,10 @@ func (ctx *rollDPoSCtx) Broadcast(endorsedMsg interface{}) {
 	ecm, ok := endorsedMsg.(*EndorsedConsensusMessage)
 	if !ok {
 		ctx.loggerWithStats().Error("invalid message type", zap.Any("message", ecm))
+	}
+	if ecm == nil {
+		ctx.loggerWithStats().Error("EndorsedConsensusMessage is nil")
+		return
 	}
 	msg, err := ecm.Proto()
 	if err != nil {
