784e89de6c3536ca9d26d0d6c0b75b9ab0aeb6a8
lzxm160 <lzxm160@sina.com>
2019-06-28 12:05:25 +0800

    x

--- action/protocol/execution/evm/evm.go
+++ action/protocol/execution/evm/evm.go
@@ -243,6 +243,7 @@ func executeInEVM(evmParams *Params, stateDB *StateDBAdapter, gasLimit uint64) (
 		// create contract
 		var evmContractAddress common.Address
 		_, evmContractAddress, remainingGas, evmErr = evm.Create(executor, evmParams.data, remainingGas, evmParams.amount)
+		fmt.Println("remaining gas 246:", remainingGas)
 		log.L().Debug("evm Create.", log.Hex("addrHash", evmContractAddress[:]))
 		if evmErr == nil {
 			if contractAddress, err := address.FromBytes(evmContractAddress.Bytes()); err == nil {
