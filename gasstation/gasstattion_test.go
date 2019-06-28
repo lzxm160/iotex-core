afcfb93095ad34cfe7e0623baa6937090458bbda
lzxm160 <lzxm160@sina.com>
2019-06-28 10:41:55 +0800

    x

--- gasstation/gasstattion_test.go
+++ gasstation/gasstattion_test.go
@@ -233,7 +233,8 @@ func getActionWithPayload() (act *iotextypes.Action) {
 	return
 }
 func getActionWithPayloadWithoutBalance() (act *iotextypes.Action) {
-	pubKey1 := identityset.PrivateKey(20).PublicKey()
+	//pubKey1 := identityset.PrivateKey(20).PublicKey()
+	pubKey1 := identityset.PrivateKey(28).PublicKey()
 	exec, _ := action.NewExecution(
 		"",
 		1,
