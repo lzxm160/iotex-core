df90db675891b3cfde6effd949a8372a34ef249d
lzxm160 <lzxm160@sina.com>
2019-06-28 13:31:11 +0800

    x

--- gasstation/gasstattion_test.go
+++ gasstation/gasstattion_test.go
@@ -111,7 +111,7 @@ func TestSuggestGasPriceForUserAction(t *testing.T) {
 	require.NotNil(t, act)
 	ret, err := gs.EstimateGasForAction(act)
 	require.NoError(t, err)
-	require.Equal(t, 1000000, ret)
+	require.Equal(t, int(1000000), ret)
 }
 
 func TestSuggestGasPriceForSystemAction(t *testing.T) {
