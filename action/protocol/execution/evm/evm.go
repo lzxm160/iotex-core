0fb99dcdd2ff64d788eb1aa899a0f18fed563a0c
lzxm160 <lzxm160@sina.com>
2019-07-03 10:33:43 +0800

    x

--- action/protocol/execution/evm/evm.go
+++ action/protocol/execution/evm/evm.go
@@ -8,6 +8,7 @@ package evm
 
 import (
 	"context"
+	"fmt"
 	"math"
 	"math/big"
 
@@ -213,6 +214,8 @@ func getChainConfig() *params.ChainConfig {
 
 func executeInEVM(evmParams *Params, stateDB *StateDBAdapter, gasLimit uint64) ([]byte, uint64, uint64, string, bool, error) {
 	remainingGas := evmParams.gas
+	fmt.Println(gasLimit)
+	fmt.Println(remainingGas)
 	if err := securityDeposit(evmParams, stateDB, gasLimit); err != nil {
 		log.L().Warn("unexpected error: not enough security deposit", zap.Error(err))
 		return nil, 0, 0, action.EmptyAddress, true, err
