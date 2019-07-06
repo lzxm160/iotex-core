7eb93a55594be68aea93f703f0fa7379af7ae186
lzxm160 <lzxm160@sina.com>
2019-07-06 15:37:10 +0800

    x

--- consensus/scheme/rolldpos/roundcalculator_test.go
+++ consensus/scheme/rolldpos/roundcalculator_test.go
@@ -35,11 +35,16 @@ func TestNewRound(t *testing.T) {
 	bc, roll := makeChain(t)
 	rc := &roundCalculator{bc, time.Second, time.Second, true, roll, bc.CandidatesByHeight}
 	proposer, err := rc.calculateProposer(5, 1, []string{"1", "2", "3", "4", "5"})
+	require.Error(err)
+
+	validDelegates := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"}
+	proposer, err = rc.calculateProposer(5, 1, validDelegates)
 	require.NoError(err)
+	require.Equal("1", proposer)
 	require.Equal("2", proposer)
 
 	rc.timeBasedRotation = false
-	proposer, err = rc.calculateProposer(5, 1, []string{"1", "2", "3", "4", "5"})
+	proposer, err = rc.calculateProposer(5, 1, validDelegates)
 	require.NoError(err)
 	require.Equal("1", proposer)
 	//ra, err := rc.NewRound(5, time.Now())
