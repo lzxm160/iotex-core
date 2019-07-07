d444d1a05820e0b6e63c58999264e992cb428ef8
lzxm160 <lzxm160@sina.com>
2019-07-07 09:29:29 +0800

    x

--- consensus/scheme/rolldpos/roundcalculator_test.go
+++ consensus/scheme/rolldpos/roundcalculator_test.go
@@ -8,6 +8,7 @@ package rolldpos
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -81,6 +82,7 @@ func TestNewRound(t *testing.T) {
 	require.NoError(err)
 	require.Equal(uint32(19), ra.roundNum)
 	require.Equal(uint64(1), ra.height)
+	fmt.Println(ra)
 	require.Equal(identityset.Address(20).String(), ra.proposer)
 }
 func TestDelegates(t *testing.T) {
