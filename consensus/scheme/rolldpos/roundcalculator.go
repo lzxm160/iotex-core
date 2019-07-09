4a9a79236abdf64659cddff282888c19959848e0
lzxm160 <lzxm160@sina.com>
2019-07-09 10:09:46 +0800

    x

--- consensus/scheme/rolldpos/roundcalculator.go
+++ consensus/scheme/rolldpos/roundcalculator.go
@@ -196,6 +196,7 @@ func (c *roundCalculator) NewRound(
 	height uint64,
 	now time.Time,
 ) (round *roundCtx, err error) {
+	fmt.Println("/////////////////////::1")
 	return c.newRound(height, now, false)
 }
 
@@ -210,7 +211,7 @@ func (c *roundCalculator) newRound(
 	var roundNum uint32
 	var proposer string
 	var roundStartTime time.Time
-	fmt.Println("/////////////////////::1")
+
 	if height != 0 {
 		epochNum = c.rp.GetEpochNum(height)
 		epochStartHeight := c.rp.GetEpochHeight(epochNum)
