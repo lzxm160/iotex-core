63721609580eb6927ccdf2c9fb8e43ac4f4ab21d
lzxm160 <lzxm160@sina.com>
2019-07-09 10:12:55 +0800

    x

--- consensus/scheme/rolldpos/roundcalculator.go
+++ consensus/scheme/rolldpos/roundcalculator.go
@@ -189,6 +189,7 @@ func (c *roundCalculator) NewRoundWithToleration(
 	height uint64,
 	now time.Time,
 ) (round *roundCtx, err error) {
+	fmt.Println("/////////////////////::1")
 	return c.newRound(height, now, true)
 }
 
@@ -196,7 +197,6 @@ func (c *roundCalculator) NewRound(
 	height uint64,
 	now time.Time,
 ) (round *roundCtx, err error) {
-	fmt.Println("/////////////////////::1")
 	return c.newRound(height, now, false)
 }
 
