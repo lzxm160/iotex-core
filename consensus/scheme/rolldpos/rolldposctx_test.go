8c24520d062e946b63ef1e3ba2aa34d244c6e4c7
lzxm160 <lzxm160@sina.com>
2019-07-10 15:51:08 +0800

    x

--- consensus/scheme/rolldpos/rolldposctx_test.go
+++ consensus/scheme/rolldpos/rolldposctx_test.go
@@ -118,7 +118,7 @@ func TestCheckBlockProposer(t *testing.T) {
 	require.Error(rctx.CheckBlockProposer(21, bp, en))
 
 	// case 5:
-	en = endorsement.NewEndorsement(time.Unix(1562382492, 0), identityset.PrivateKey(4).PublicKey(), nil)
+	en = endorsement.NewEndorsement(time.Unix(1562382492, 0), identityset.PrivateKey(6).PublicKey(), nil)
 	err := rctx.CheckBlockProposer(21, bp, en)
 	fmt.Println(err)
 }
