69ff48da15f09cbcdbfe3f96bc1d7762784ca912
lzxm160 <lzxm160@sina.com>
2019-07-10 17:13:38 +0800

    x

--- consensus/scheme/rolldpos/rolldposctx_test.go
+++ consensus/scheme/rolldpos/rolldposctx_test.go
@@ -136,8 +136,8 @@ func TestCheckBlockProposer(t *testing.T) {
 	en = endorsement.NewEndorsement(time.Unix(1562382392, 0), identityset.PrivateKey(5).PublicKey(), nil)
 	en2 := endorsement.NewEndorsement(time.Unix(1562382392, 0), identityset.PrivateKey(10).PublicKey(), nil)
 	bp = newBlockProposal(&block, []*endorsement.Endorsement{en2, en})
-	require.Error(rctx.CheckBlockProposer(21, bp, en))
-	fmt.Println(rctx.CheckBlockProposer(21, bp, en))
+	require.Error(rctx.CheckBlockProposer(21, bp, en2))
+	fmt.Println(rctx.CheckBlockProposer(21, bp, en2))
 
 	// case 8:normal
 	block = getBlockforctx(t, 5, true)
