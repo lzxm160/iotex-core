32ed3f00c486d0fd765d096f8598774c9192c898
lzxm160 <lzxm160@sina.com>
2019-07-11 10:12:23 +0800

    x

--- api/api.go
+++ api/api.go
@@ -739,45 +739,45 @@ func (api *Server) getActionsByAddress(address string, start uint64, count uint6
 		return nil, status.Error(codes.InvalidArgument, "range exceeds the limit")
 	}
 
-	actions, err := api.getTotalActionsByAddress(address)
+	actions, err := api.getTotalActionsByAddress(address, start, count)
 	if err != nil {
 		return nil, status.Error(codes.NotFound, err.Error())
 	}
 	if len(actions) == 0 {
 		return &iotexapi.GetActionsResponse{}, nil
 	}
-	if start >= uint64(len(actions)) {
-		return nil, status.Error(codes.InvalidArgument, "start exceeds the limit")
-	}
+	//if start >= uint64(len(actions)) {
+	//	return nil, status.Error(codes.InvalidArgument, "start exceeds the limit")
+	//}
 
 	res := &iotexapi.GetActionsResponse{Total: uint64(len(actions))}
 
-	account, err := api.bc.StateByAddr(address)
-	if err != nil {
-		return nil, status.Error(codes.Internal, "failed to get account state by address")
-	}
+	//account, err := api.bc.StateByAddr(address)
+	//if err != nil {
+	//	return nil, status.Error(codes.Internal, "failed to get account state by address")
+	//}
 
 	// In the case of contract address, do not sort in order to save db I/O performance
 	// TODO: This is a workaround to keep actions in correct order while not affecting db I/O performance too much
-	if account.IsContract() {
-		for i := start; i < uint64(len(actions)) && i < start+count; i++ {
-			act, err := api.getAction(actions[i], false)
-			if err != nil {
-				continue
-			}
-			res.ActionInfo = append(res.ActionInfo, act)
-		}
-		return res, nil
-	}
-
-	// sort action by timestamp
-	for _, action := range actions {
-		act, err := api.getAction(action, false)
+	//if account.IsContract() {
+	for i := start; i < uint64(len(actions)) && i < start+count; i++ {
+		act, err := api.getAction(actions[i], false)
 		if err != nil {
 			continue
 		}
 		res.ActionInfo = append(res.ActionInfo, act)
 	}
+	return res, nil
+	//}
+
+	// sort action by timestamp
+	//for _, action := range actions {
+	//	act, err := api.getAction(action, false)
+	//	if err != nil {
+	//		continue
+	//	}
+	//	res.ActionInfo = append(res.ActionInfo, act)
+	//}
 	//sort.Slice(res.ActionInfo, func(i, j int) bool {
 	//	return res.ActionInfo[i].Timestamp.Seconds < res.ActionInfo[j].Timestamp.Seconds
 	//})
@@ -998,7 +998,7 @@ func (api *Server) getAction(actHash hash.Hash256, checkPending bool) (*iotexapi
 	return api.pendingAction(selp)
 }
 
-func (api *Server) getTotalActionsByAddress(address string) ([]hash.Hash256, error) {
+func (api *Server) getTotalActionsByAddress(address string, start uint64, count uint64) ([]hash.Hash256, error) {
 	actions, err := api.bc.GetActionsFromAddress(address)
 	if err != nil {
 		return nil, err
