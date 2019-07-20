229a0cdf457243d23a70e35a4029aaeaade41c60
lzxm160 <lzxm160@sina.com>
2019-07-20 09:38:00 +0800

    x

--- blockchain/indexbuilder.go
+++ blockchain/indexbuilder.go
@@ -163,8 +163,12 @@ func (ib *IndexBuilder) commitBatchAndClear(tipIndex, tipHeight uint64, batch db
 	if err := ib.store.Commit(batch); err != nil {
 		return err
 	}
-	actDelta.senderDelta = make(map[hash.Hash160]uint64)
-	actDelta.recipientDelta = make(map[hash.Hash160]uint64)
+	for k, _ := range actDelta.senderDelta {
+		actDelta.senderDelta[k] = 0
+	}
+	for k, _ := range actDelta.recipientDelta {
+		actDelta.recipientDelta[k] = 0
+	}
 	return nil
 }
 func (ib *IndexBuilder) initAndLoadActions() error {
