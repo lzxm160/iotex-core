36d8233ba9c1e0cf367875589b3ca535b2e60030
lzxm160 <lzxm160@sina.com>
2019-07-23 15:28:45 +0800

    x

--- db/db_bolt.go
+++ db/db_bolt.go
@@ -166,7 +166,7 @@ func (b *boltDB) Commit(batch KVStoreBatch) (err error) {
 
 // DeleteBucket delete namespace
 func (b *boltDB) DeleteBucket(key []byte) (err error) {
-	return b.db.View(func(tx *bolt.Tx) error {
+	return b.db.Update(func(tx *bolt.Tx) error {
 		if err := tx.DeleteBucket(key); err != bolt.ErrTxNotWritable {
 			return err
 		}
