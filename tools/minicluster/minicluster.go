3a09e7c27cdc1a28c09286904b1cf5efca82e3b3
lzxm160 <lzxm160@sina.com>
2019-07-29 15:38:14 +0800

    x

--- tools/minicluster/minicluster.go
+++ tools/minicluster/minicluster.go
@@ -407,5 +407,7 @@ func newConfig(
 	cfg.Genesis.Delegates = cfg.Genesis.Delegates[3 : numNodes+3]
 	cfg.Genesis.EnableGravityChainVoting = true
 	cfg.System.EnableExperimentalActions = true
+	logFile := fmt.Sprintf("%d.log", networkPort)
+	cfg.Log.StderrRedirectFile = &logFile
 	return cfg
 }
