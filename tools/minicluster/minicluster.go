1fb24f54eca61727c06a5e4041796519ff85a14b
lzxm160 <lzxm160@sina.com>
2019-07-29 16:54:19 +0800

    x

--- tools/minicluster/minicluster.go
+++ tools/minicluster/minicluster.go
@@ -419,7 +419,7 @@ func newConfig(
 	logFile := fmt.Sprintf("%d.log", networkPort)
 	cfg.Log.StderrRedirectFile = &logFile
 	cfg.Log.RedirectStdLog = true
-	fmt.Println(cfg)
+	fmt.Println(cfg.Log)
 	//cfg.Log.Zap.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
 	//cfg.Log.Zap.Encoding = "json"
 	//cfg.Log.Zap.DisableStacktrace = true
