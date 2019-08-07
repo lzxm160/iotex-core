3c0d0ecabe38b03d4341092a8cdb057b89026cf6
lzxm160 <lzxm160@sina.com>
2019-08-07 13:57:46 +0800

    x

--- state/factory/statedb.go
+++ state/factory/statedb.go
@@ -271,7 +271,7 @@ func (sdb *stateDB) stateHeight(addr hash.Hash160, height uint64, s interface{})
 	if maxVersion == 0 || height > maxVersion {
 		return errors.New("cannot find state")
 	}
-
+	log.L().Info("////////////////", zap.Uint64("maxVersion", maxVersion))
 	db := sdb.dao.DB()
 	boltdb, ok := db.(*bolt.DB)
 	if !ok {
@@ -283,6 +283,7 @@ func (sdb *stateDB) stateHeight(addr hash.Hash160, height uint64, s interface{})
 		binary.BigEndian.PutUint64(bytess, maxVersion)
 		stateKey := append(addr[:], bytess...)
 		for k, v := c.Seek(stateKey); k != nil && bytes.Compare(k, stateKey) <= 0; k, v = c.Prev() {
+			log.L().Info("////////////////", zap.Uint64("k", binary.BigEndian.Uint64(k[20:])))
 			if bytes.Compare(heightKey, k) >= 0 {
 				if err := state.Deserialize(s, v); err != nil {
 					return errors.Wrapf(err, "error when deserializing state data into %T", s)
@@ -297,7 +298,7 @@ func (sdb *stateDB) stateHeight(addr hash.Hash160, height uint64, s interface{})
 
 func (sdb *stateDB) accountState(encodedAddrs string) (account *state.Account, err error) {
 	// TODO: state db shouldn't serve this function
-	fmt.Println("////////////////", encodedAddrs)
+	log.L().Info("////////////////", zap.String("address", encodedAddrs))
 	var encodedAddr string
 	height := uint64(0)
 	if len(encodedAddrs) > 41 {
@@ -309,7 +310,7 @@ func (sdb *stateDB) accountState(encodedAddrs string) (account *state.Account, e
 	} else {
 		encodedAddr = encodedAddrs
 	}
-	fmt.Println("////////////////", encodedAddr)
+	log.L().Info("////////////////", zap.Uint64("height", height))
 	addr, err := address.FromString(encodedAddr)
 	if err != nil {
 		return nil, err
@@ -320,6 +321,7 @@ func (sdb *stateDB) accountState(encodedAddrs string) (account *state.Account, e
 	account = &acc
 	if height != 0 {
 		err = sdb.stateHeight(pkHash, height, account)
+		log.L().Info("////////////////stateHeight ", zap.Error(err))
 	} else {
 		fmt.Println("//////////////////320here")
 		err = sdb.state(pkHash, account)
