6ac8e525e0dc20f0f07a3ac9edebf92be2b16d92
lzxm160 <lzxm160@sina.com>
2019-08-20 21:48:34 +0800

    x

--- api/api.go
+++ api/api.go
@@ -724,16 +724,16 @@ func (api *Server) getstorageAt(ws protocol.StateManager, args ...[]byte) (res *
 		return nil, err
 	}
 	log.L().Info("account root:", zap.String("root", hex.EncodeToString(acc.Root[:])))
-	//dao := ws.GetDB()
+	dao := ws.GetDB()
 	//batch := ws.GetCachedBatch()
-	cfg := api.cfg.DB
-	//cfg.DbPath = api.cfg.Chain.TrieDBPath
-	cfg.DbPath = "/var/data/triebak.db"
-	log.L().Info("path:", zap.String("path", cfg.DbPath))
-	dao := db.NewBoltDB(cfg)
-	dao.Start(context.Background())
-	log.L().Info("dao start:")
-	defer dao.Stop(context.Background())
+	//cfg := api.cfg.DB
+	////cfg.DbPath = api.cfg.Chain.TrieDBPath
+	//cfg.DbPath = "/var/data/triebak.db"
+	//log.L().Info("path:", zap.String("path", cfg.DbPath))
+	//dao := db.NewBoltDB(cfg)
+	//dao.Start(context.Background())
+	//log.L().Info("dao start:")
+	//defer dao.Stop(context.Background())
 	dbForTrie, err := db.NewKVStoreForTrie(evm.ContractKVNameSpace, dao, db.CachedBatchOption(db.NewCachedBatch()))
 	if err != nil {
 		return nil, err
