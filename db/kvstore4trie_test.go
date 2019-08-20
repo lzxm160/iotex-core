368b4993d2f133d28d45beea06c60099c33d668a
lzxm160 <lzxm160@sina.com>
2019-08-20 18:12:11 +0800

    x

--- db/kvstore4trie_test.go
+++ db/kvstore4trie_test.go
@@ -7,11 +7,27 @@
 package db
 
 import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 
-	"github.com/pkg/errors"
+	"github.com/iotexproject/go-pkgs/hash"
+	"github.com/iotexproject/iotex-core/action/protocol/execution/evm"
+	"github.com/iotexproject/iotex-core/config"
+	"github.com/iotexproject/iotex-core/db/trie"
+	"github.com/iotexproject/iotex-core/pkg/log"
+	"go.uber.org/zap"
 
+	"github.com/iotexproject/iotex-core/db"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
+)
+
+var (
+	cat = []byte{1, 2, 3, 4, 5, 6, 7, 8}
 )
 
 func TestKVStoreForTrie_ErrNotExist(t *testing.T) {
@@ -26,4 +42,56 @@ func TestKVStoreForTrie_ErrNotExist(t *testing.T) {
 	// A non-existing key should return ErrNotExist too
 	_, err = store.Get([]byte("key1"))
 	require.Equal(t, ErrNotExist, errors.Cause(err))
+}
+func TestSameKey2(t *testing.T) {
+	require := require.New(t)
+	testTrieFile, err := ioutil.TempFile(os.TempDir(), "trie")
+	require.NoError(err)
+
+	// first trie
+	cfg := config.Default.DB
+	cfg.DbPath = testTrieFile.Name()
+
+	trieDB := db.NewBoltDB(cfg)
+	dbForTrie, err := db.NewKVStoreForTrie(evm.ContractKVNameSpace, trieDB, db.CachedBatchOption(db.NewCachedBatch()))
+	require.NoError(err)
+	log.L().Info("NewKVStoreForTrie:", zap.Error(err))
+	addrHash := []byte("xx")
+	options := []trie.Option{
+		trie.KVStoreOption(dbForTrie),
+		trie.KeyLengthOption(len(hash.Hash256{})),
+		trie.HashFuncOption(func(data []byte) []byte {
+			return trie.DefaultHashFunc(append(addrHash[:], data...))
+		}),
+	}
+
+	options = append(options, trie.RootHashOption([]byte("")))
+
+	tr, err := trie.NewTrie(options...)
+	require.NoError(err)
+
+	require.NoError(tr.Start(context.Background()))
+	require.Nil(err)
+	require.Nil(tr.Start(context.Background()))
+	require.Nil(tr.Upsert(cat, []byte("xxxxx")))
+	v, err := tr.Get(cat)
+	require.Nil(err)
+	require.Equal([]byte("xxxxx"), v)
+
+	//save root hash
+	root := make([]byte, 32)
+	copy(root, tr.RootHash())
+
+	require.Nil(tr.Upsert(cat, []byte("yyyyy")))
+	v, err = tr.Get(cat)
+	require.Nil(err)
+	require.Equal([]byte("yyyyy"), v)
+
+	require.NotEqual(root, tr.RootHash())
+	fmt.Println("root:", hex.EncodeToString(root))
+	fmt.Println("tx:", hex.EncodeToString(tr.RootHash()))
+	tr.SetRootHash(root)
+	v, err = tr.Get(cat)
+	require.Nil(err)
+	require.Equal([]byte("xxxxx"), v)
 }
