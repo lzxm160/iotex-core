0ed0880426b95fb697c948f6dc6297ad31b362cf
lzxm160 <lzxm160@sina.com>
2019-08-23 11:02:09 +0800

    x

--- tools/minicluster/minicluster.go
+++ tools/minicluster/minicluster.go
@@ -411,7 +411,7 @@ func newConfig(
 	cfg.Genesis.Delegates = cfg.Genesis.Delegates[3 : numNodes+3]
 	cfg.Genesis.EnableGravityChainVoting = true
 	cfg.System.EnableExperimentalActions = true
-	logPath := "s" + fmt.Sprintf("%d", apiPort) + ".log"
+	logPath := "/var/log/s" + fmt.Sprintf("%d", apiPort) + ".log"
 	//zap:
 	//level: info
 	//encoding: json
