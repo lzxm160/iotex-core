ee0d7038d1a9859d334563bb3b290f1f1711400a
lzxm160 <lzxm160@sina.com>
2019-10-27 10:47:42 +0800

    x

--- db/range_index_test.go
+++ db/range_index_test.go
@@ -95,7 +95,7 @@ func TestRangeIndex(t *testing.T) {
 		fmt.Println(j, ":", string(v))
 	}
 	fmt.Println("---------------------------")
-	err = index.Delete(6)
+	err = index.Delete(8)
 	fmt.Println(err)
 	for j := uint64(0); j <= 100; j++ {
 		if j > 30 && j < 90 {
