d68802bcf9bd8699d9320906e18655b9c903002f
lzxm160 <lzxm160@sina.com>
2019-10-31 18:17:11 +0800

    x

--- state/factory/factory.go
+++ state/factory/factory.go
@@ -265,6 +265,7 @@ func (sf *factory) NewWorkingSet(saveHistory bool) (WorkingSet, error) {
 
 // Commit persists all changes in RunActions() into the DB
 func (sf *factory) Commit(ws WorkingSet) error {
+	log.L().Info("(sf *factory) Commit(ws WorkingSet) error {", zap.Error(errors.New("who is calling me ")))
 	if ws == nil {
 		return errors.New("working set doesn't exist")
 	}
