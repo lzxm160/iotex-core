60ac563ed3f19f22248d34ab4b2233ab0065fb51
lzxm160 <lzxm160@sina.com>
2019-11-06 15:36:12 +0800

    x

--- api/api_test.go
+++ api/api_test.go
@@ -9,15 +9,13 @@ package api
 import (
 	"context"
 	"encoding/hex"
+	"errors"
 	"io/ioutil"
 	"math/big"
 	"os"
 	"strconv"
 	"testing"
 	"time"
-
-	"github.com/iotexproject/iotex-core/action/protocol/account"
-	"github.com/iotexproject/iotex-core/action/protocol/rolldpos"
 
 	"github.com/golang/mock/gomock"
 	"github.com/golang/protobuf/proto"
@@ -27,14 +25,19 @@ import (
 
 	"github.com/iotexproject/iotex-core/action"
 	"github.com/iotexproject/iotex-core/action/protocol"
+	"github.com/iotexproject/iotex-core/action/protocol/account"
 	accountutil "github.com/iotexproject/iotex-core/action/protocol/account/util"
 	"github.com/iotexproject/iotex-core/action/protocol/execution"
 	"github.com/iotexproject/iotex-core/action/protocol/poll"
 	"github.com/iotexproject/iotex-core/action/protocol/rewarding"
+	"github.com/iotexproject/iotex-core/action/protocol/rolldpos"
 	"github.com/iotexproject/iotex-core/actpool"
 	"github.com/iotexproject/iotex-core/blockchain"
+	"github.com/iotexproject/iotex-core/blockchain/blockdao"
 	"github.com/iotexproject/iotex-core/blockchain/genesis"
+	"github.com/iotexproject/iotex-core/blockindex"
 	"github.com/iotexproject/iotex-core/config"
+	"github.com/iotexproject/iotex-core/db"
 	"github.com/iotexproject/iotex-core/gasstation"
 	"github.com/iotexproject/iotex-core/pkg/unit"
 	"github.com/iotexproject/iotex-core/pkg/util/byteutil"
@@ -1750,7 +1753,7 @@ func newConfig() config.Config {
 
 func createServer(cfg config.Config, needActPool bool) (*Server, error) {
 	cfg.Chain.ProducerPrivKey = hex.EncodeToString(identityset.PrivateKey(0).Bytes())
-	bc, dao, indexer, registry, _, err := blockchain.CreateBlockchain(true, cfg, []string{rolldpos.ProtocolID, account.ProtocolID, execution.ProtocolID, rewarding.ProtocolID, poll.ProtocolID})
+	bc, dao, indexer, registry, _, err := CreateBlockchain(true, cfg, []string{rolldpos.ProtocolID, account.ProtocolID, execution.ProtocolID, rewarding.ProtocolID, poll.ProtocolID})
 	if err != nil {
 		return nil, err
 	}
@@ -1796,4 +1799,108 @@ func createServer(cfg config.Config, needActPool bool) (*Server, error) {
 	}
 
 	return svr, nil
+}
+
+func CreateBlockchain(inMem bool, cfg config.Config, protocols []string) (bc blockchain.Blockchain, dao blockdao.BlockDAO, indexer blockindex.Indexer, registry *protocol.Registry, sf factory.Factory, err error) {
+	if inMem {
+		sf, err = factory.NewFactory(cfg, factory.InMemTrieOption())
+		if err != nil {
+			return
+		}
+	} else {
+		sf, err = factory.NewFactory(cfg, factory.DefaultTrieOption())
+		if err != nil {
+			return
+		}
+	}
+	var indexerDB, blockdaoDB db.KVStore
+	if inMem {
+		indexerDB = db.NewMemKVStore()
+		blockdaoDB = db.NewMemKVStore()
+	} else {
+		cfg.DB.DbPath = cfg.Chain.IndexDBPath
+		indexerDB = db.NewBoltDB(cfg.DB)
+		cfg.DB.DbPath = cfg.Chain.ChainDBPath
+		blockdaoDB = db.NewBoltDB(cfg.DB)
+	}
+	// create indexer
+	indexer, err = blockindex.NewIndexer(indexerDB, cfg.Genesis.Hash())
+	if err != nil {
+		return
+	}
+	// create BlockDAO
+	dao = blockdao.NewBlockDAO(blockdaoDB, indexer, cfg.Chain.CompressBlock, cfg.DB)
+	if dao == nil {
+		err = errors.New("failed to create blockdao")
+		return
+	}
+	// create chain
+	registry = &protocol.Registry{}
+	bc = blockchain.NewBlockchain(
+		cfg,
+		dao,
+		blockchain.PrecreatedStateFactoryOption(sf),
+		blockchain.RegistryOption(registry),
+	)
+	if bc == nil {
+		err = errors.New("failed to create blockchain")
+		return
+	}
+
+	var reward, acc, evm protocol.Protocol
+	var rolldposProtocol *rolldpos.Protocol
+	var haveReward bool
+	for _, proto := range protocols {
+		switch proto {
+		case rolldpos.ProtocolID:
+			rolldposProtocol = rolldpos.NewProtocol(
+				cfg.Genesis.NumCandidateDelegates,
+				cfg.Genesis.NumDelegates,
+				cfg.Genesis.NumSubEpochs,
+			)
+			if err = registry.Register(rolldpos.ProtocolID, rolldposProtocol); err != nil {
+				return
+			}
+
+		case account.ProtocolID:
+			acc = account.NewProtocol(config.NewHeightUpgrade(cfg))
+			if err = registry.Register(account.ProtocolID, acc); err != nil {
+				return
+			}
+			sf.AddActionHandlers(acc)
+		case execution.ProtocolID:
+			evm = execution.NewProtocol(bc, config.NewHeightUpgrade(cfg))
+			if err = registry.Register(execution.ProtocolID, evm); err != nil {
+				return
+			}
+			sf.AddActionHandlers(evm)
+		case rewarding.ProtocolID:
+			haveReward = true
+		case poll.ProtocolID:
+			p := poll.NewLifeLongDelegatesProtocol(cfg.Genesis.Delegates)
+			if err = registry.Register(poll.ProtocolID, p); err != nil {
+				return
+			}
+		}
+	}
+
+	if haveReward && rolldposProtocol != nil {
+		reward = rewarding.NewProtocol(bc, rolldposProtocol)
+		if err = registry.Register(rewarding.ProtocolID, reward); err != nil {
+			return
+		}
+		sf.AddActionHandlers(reward)
+	}
+
+	bc.Validator().AddActionEnvelopeValidators(protocol.NewGenericValidator(bc))
+	if acc != nil {
+		bc.Validator().AddActionValidators(acc)
+	}
+	if evm != nil {
+		bc.Validator().AddActionValidators(evm)
+	}
+	if reward != nil {
+		bc.Validator().AddActionValidators(reward)
+	}
+	return
 }
--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -25,7 +25,7 @@ import (
 	"github.com/iotexproject/iotex-core/action"
 	"github.com/iotexproject/iotex-core/action/protocol"
 	"github.com/iotexproject/iotex-core/action/protocol/account"
-	"github.com/iotexproject/iotex-core/action/protocol/account/util"
+	accountutil "github.com/iotexproject/iotex-core/action/protocol/account/util"
 	"github.com/iotexproject/iotex-core/action/protocol/execution"
 	"github.com/iotexproject/iotex-core/action/protocol/poll"
 	"github.com/iotexproject/iotex-core/action/protocol/rewarding"
@@ -33,7 +33,9 @@ import (
 	"github.com/iotexproject/iotex-core/blockchain/block"
 	"github.com/iotexproject/iotex-core/blockchain/blockdao"
 	"github.com/iotexproject/iotex-core/blockchain/genesis"
+	"github.com/iotexproject/iotex-core/blockindex"
 	"github.com/iotexproject/iotex-core/config"
+	"github.com/iotexproject/iotex-core/db"
 	"github.com/iotexproject/iotex-core/pkg/unit"
 	"github.com/iotexproject/iotex-core/state/factory"
 	"github.com/iotexproject/iotex-core/test/identityset"
@@ -1135,4 +1137,108 @@ func addCreatorToFactory(sf factory.Factory) error {
 		return err
 	}
 	return sf.Commit(ws)
+}
+
+func CreateBlockchain(inMem bool, cfg config.Config, protocols []string) (bc Blockchain, dao blockdao.BlockDAO, indexer blockindex.Indexer, registry *protocol.Registry, sf factory.Factory, err error) {
+	if inMem {
+		sf, err = factory.NewFactory(cfg, factory.InMemTrieOption())
+		if err != nil {
+			return
+		}
+	} else {
+		sf, err = factory.NewFactory(cfg, factory.DefaultTrieOption())
+		if err != nil {
+			return
+		}
+	}
+	var indexerDB, blockdaoDB db.KVStore
+	if inMem {
+		indexerDB = db.NewMemKVStore()
+		blockdaoDB = db.NewMemKVStore()
+	} else {
+		cfg.DB.DbPath = cfg.Chain.IndexDBPath
+		indexerDB = db.NewBoltDB(cfg.DB)
+		cfg.DB.DbPath = cfg.Chain.ChainDBPath
+		blockdaoDB = db.NewBoltDB(cfg.DB)
+	}
+	// create indexer
+	indexer, err = blockindex.NewIndexer(indexerDB, cfg.Genesis.Hash())
+	if err != nil {
+		return
+	}
+	// create BlockDAO
+	dao = blockdao.NewBlockDAO(blockdaoDB, indexer, cfg.Chain.CompressBlock, cfg.DB)
+	if dao == nil {
+		err = errors.New("failed to create blockdao")
+		return
+	}
+	// create chain
+	registry = &protocol.Registry{}
+	bc = NewBlockchain(
+		cfg,
+		dao,
+		PrecreatedStateFactoryOption(sf),
+		RegistryOption(registry),
+	)
+	if bc == nil {
+		err = errors.New("failed to create blockchain")
+		return
+	}
+
+	var reward, acc, evm protocol.Protocol
+	var rolldposProtocol *rolldpos.Protocol
+	var haveReward bool
+	for _, proto := range protocols {
+		switch proto {
+		case rolldpos.ProtocolID:
+			rolldposProtocol = rolldpos.NewProtocol(
+				cfg.Genesis.NumCandidateDelegates,
+				cfg.Genesis.NumDelegates,
+				cfg.Genesis.NumSubEpochs,
+			)
+			if err = registry.Register(rolldpos.ProtocolID, rolldposProtocol); err != nil {
+				return
+			}
+
+		case account.ProtocolID:
+			acc = account.NewProtocol(config.NewHeightUpgrade(cfg))
+			if err = registry.Register(account.ProtocolID, acc); err != nil {
+				return
+			}
+			sf.AddActionHandlers(acc)
+		case execution.ProtocolID:
+			evm = execution.NewProtocol(bc, config.NewHeightUpgrade(cfg))
+			if err = registry.Register(execution.ProtocolID, evm); err != nil {
+				return
+			}
+			sf.AddActionHandlers(evm)
+		case rewarding.ProtocolID:
+			haveReward = true
+		case poll.ProtocolID:
+			p := poll.NewLifeLongDelegatesProtocol(cfg.Genesis.Delegates)
+			if err = registry.Register(poll.ProtocolID, p); err != nil {
+				return
+			}
+		}
+	}
+
+	if haveReward && rolldposProtocol != nil {
+		reward = rewarding.NewProtocol(bc, rolldposProtocol)
+		if err = registry.Register(rewarding.ProtocolID, reward); err != nil {
+			return
+		}
+		sf.AddActionHandlers(reward)
+	}
+
+	bc.Validator().AddActionEnvelopeValidators(protocol.NewGenericValidator(bc))
+	if acc != nil {
+		bc.Validator().AddActionValidators(acc)
+	}
+	if evm != nil {
+		bc.Validator().AddActionValidators(evm)
+	}
+	if reward != nil {
+		bc.Validator().AddActionValidators(reward)
+	}
+	return
 }
