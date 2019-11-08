52c6c8803ce6798330af8e402a94b2d0f82a0403
lzxm160 <lzxm160@sina.com>
2019-11-08 14:33:51 +0800

    x

--- state/factory/statetx.go
+++ state/factory/statetx.go
@@ -218,7 +218,7 @@ func (stx *stateTX) putIndex(pkHash hash.Hash160, ss []byte) error {
 	if err != nil {
 		return err
 	}
-	log.L().Info("ri.Insert(version, ss)", zap.Uint64("version", version))
+	//log.L().Info("ri.Insert(version, ss)", zap.Uint64("version", version))
 	return ri.Insert(version, ss)
 }
 
