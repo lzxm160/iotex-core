a4796704dd5068378491f0842be82ba91b922a9d
lzxm160 <lzxm160@sina.com>
2019-11-19 13:31:32 +0800

    x

--- blockchain/blockdao/blockdao.go
+++ blockchain/blockdao/blockdao.go
@@ -612,12 +612,9 @@ func (dao *blockDAO) putBlockForBlockdb(blk *block.Block) error {
 	if h != hash.ZeroHash256 && err == nil {
 		return errors.Errorf("block %d already exist", blkHeight)
 	}
-	kv, index, err := dao.getTopDB(blkHeight)
+	kv, _, err := dao.getTopDB(blkHeight)
 	if err != nil {
 		return err
-	}
-	if index == 0 {
-		kv = dao.kvstore
 	}
 	batchForBlock := db.NewBatch()
 	_, err = kv.Get(blockNS, startHeightKey)
