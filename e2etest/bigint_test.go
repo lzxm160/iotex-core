929e9ccfcf5d2b3050b1884da60476ebc63e2431
lzxm160 <lzxm160@sina.com>
2019-11-20 20:19:48 +0800

    x

--- e2etest/bigint_test.go
+++ e2etest/bigint_test.go
@@ -63,7 +63,7 @@ func TestAction_Negative(t *testing.T) {
 	r := require.New(t)
 	ctx := context.Background()
 	bc := prepareBlockchain(ctx, executor, r)
-	defer r.NoError(bc.Stop(ctx))
+	defer bc.Stop(ctx)
 	balanceBeforeTransfer, err := bc.Factory().Balance(executor)
 	r.NoError(err)
 	blk, err := prepareAction(bc, r)
