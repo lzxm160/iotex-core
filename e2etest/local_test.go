cf6312a707fe7667032c7b7f7e362f024dd5804b
lzxm160 <lzxm160@sina.com>
2019-11-21 13:29:20 +0800

    TipHeight and TipHash should not be stored in a separate db

--- e2etest/local_test.go
+++ e2etest/local_test.go
@@ -546,6 +546,7 @@ func TestStartExistingBlockchain(t *testing.T) {
 	require.NoError(svr.Stop(context.Background()))
 	//require.NoError(bc.Stop(ctx))
 	fmt.Println("555555555555555555")
+	time.Sleep(time.Second * 3)
 	ctx = context.Background()
 	require.NoError(bc.Start(ctx))
 	fmt.Println("xxssssssssssssssssssss")
