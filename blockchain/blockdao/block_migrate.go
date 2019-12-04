1340f65c6e345ce7419ba66819ee874be6a7182c
lzxm160 <lzxm160@sina.com>
2019-12-04 10:07:36 +0800

    x

--- blockchain/blockdao/block_migrate.go
+++ blockchain/blockdao/block_migrate.go
@@ -104,7 +104,9 @@ func (dao *blockDAO) migrate(oldpath string) error {
 	if err != nil {
 		return err
 	}
+
 	tipHeight := byteutil.BytesToUint64(tipHeightValue)
+	log.L().Info("migrate::", zap.Uint64("height:", tipHeight))
 	for i := uint64(1); i <= tipHeight; i++ {
 		blk, err := dao.getBlockByHeightLegacy(i)
 		if err != nil {
