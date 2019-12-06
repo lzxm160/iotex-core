0b783e727f07f652cb235a08196d2883fa9e0e48
lzxm160 <lzxm160@sina.com>
2019-12-06 10:52:03 +0800

    x

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -770,6 +770,7 @@ func (bc *blockchain) validateBlock(blk *block.Block) error {
 	if err != nil {
 		return errors.Wrap(err, "Failed to obtain working set from state factory")
 	}
+	ws.GetCachedBatch().ClearAndUnlock()
 	runTimer := bc.timerFactory.NewTimer("runActions")
 	receipts, err := bc.runActions(ctx, blk, ws)
 	runTimer.End()
