c8c5e2d2b53e96c4cb8d954a70d031f9876bdc41
lzxm160 <lzxm160@sina.com>
2020-02-24 10:00:57 +0800

    x

--- action/stakerestake_test.go
+++ action/stakerestake_test.go
@@ -66,7 +66,7 @@ func TestRestakeSignVerify(t *testing.T) {
 	require.NotNil(selp)
 	ser, err := proto.Marshal(selp.Proto())
 	require.NoError(err)
-	require.Equal("0a1c080118c0843d22023130da020f080a120231301a077061796c6f6164124104755ce6d8903f6b3793bddb4ea5d3589d637de2d209ae0ea930815c82db564ee8cc448886f639e8a0c7e94e99a5c1335b583c0bc76ef30dd6a1038ed9da8daf331a41a48ab1feba8181d760de946aefed7d815a89fd9b1ab503d2392bb55e1bb75eec42dddc8bd642f89accc3a37b3cf15a103a95d66695fdf0647b202869fdd66bcb01", hex.EncodeToString(ser))
+	require.Equal("0a1d080118c0843d22023130e20210080a10e807180122077061796c6f6164124104755ce6d8903f6b3793bddb4ea5d3589d637de2d209ae0ea930815c82db564ee8cc448886f639e8a0c7e94e99a5c1335b583c0bc76ef30dd6a1038ed9da8daf331a41e2e763aed5b1fd1a8601de0f0ae34eb05162e34b0389ae3418eedbf762f64959634a968313a6516dba3a97b34efba4753bbed3a33d409ecbd45ac75007cd8e9101", hex.EncodeToString(ser))
 	hash := selp.Hash()
 	require.Equal("ca8937d6f224a4e4bf93cb5605581de2d26fb0481e1dfc1eef384ee7ccf94b73", hex.EncodeToString(hash[:]))
 	// verify signature
