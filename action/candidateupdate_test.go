4417a914c143e3b81a7c03456619a3f0cdfed378
lzxm160 <lzxm160@sina.com>
2020-03-03 10:36:13 +0800

    x

--- action/candidateupdate_test.go
+++ action/candidateupdate_test.go
@@ -16,59 +16,57 @@ import (
 
 func TestCandidateUpdate(t *testing.T) {
 	require := require.New(t)
-	stake, err := NewRestake(nonce, index, duration, autoStake, payload, gaslimit, gasprice)
+	cr, err := NewCandidateUpdate(nonce, canAddress, canAddress, canAddress, gaslimit, gasprice)
 	require.NoError(err)
 
-	ser := stake.Serialize()
-	require.Equal("080a10e807180122077061796c6f6164", hex.EncodeToString(ser))
+	ser := cr.Serialize()
+	require.Equal("0a81010a29696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611229696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611a29696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611202313018e80720012a29696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a6132077061796c6f6164", hex.EncodeToString(ser))
 
 	require.NoError(err)
-	require.Equal(gaslimit, stake.GasLimit())
-	require.Equal(gasprice, stake.GasPrice())
-	require.Equal(nonce, stake.Nonce())
+	require.Equal(gaslimit, cr.GasLimit())
+	require.Equal(gasprice, cr.GasPrice())
+	require.Equal(nonce, cr.Nonce())
 
-	require.True(stake.AutoStake())
-	require.Equal(payload, stake.Payload())
-	require.Equal(duration, stake.Duration())
-	require.Equal(index, stake.BucketIndex())
+	require.Equal(canAddress, cr.Name())
+	require.Equal(canAddress, cr.OperatorAddress().String())
+	require.Equal(canAddress, cr.RewardAddress().String())
 
-	gas, err := stake.IntrinsicGas()
+	gas, err := cr.IntrinsicGas()
 	require.NoError(err)
 	require.Equal(uint64(10700), gas)
-	cost, err := stake.Cost()
+	cost, err := cr.Cost()
 	require.NoError(err)
-	require.Equal("107000", cost.Text(10))
+	require.Equal("107010", cost.Text(10))
 
-	proto := stake.Proto()
-	stake2 := &Restake{}
-	require.NoError(stake2.LoadProto(proto))
-	require.True(stake2.AutoStake())
-	require.Equal(payload, stake2.Payload())
-	require.Equal(duration, stake2.Duration())
-	require.Equal(index, stake2.BucketIndex())
+	proto := cr.Proto()
+	cr2 := &CandidateUpdate{}
+	require.NoError(cr2.LoadProto(proto))
+	require.Equal(canAddress, cr2.Name())
+	require.Equal(canAddress, cr2.OperatorAddress().String())
+	require.Equal(canAddress, cr2.RewardAddress().String())
 }
 
 func TestCandidateUpdateSignVerify(t *testing.T) {
 	require := require.New(t)
 	require.Equal("cfa6ef757dee2e50351620dca002d32b9c090cfda55fb81f37f1d26b273743f1", senderKey.HexString())
-	stake, err := NewRestake(nonce, index, duration, autoStake, payload, gaslimit, gasprice)
+	cr, err := NewCandidateUpdate(nonce, canAddress, canAddress, canAddress, gaslimit, gasprice)
 	require.NoError(err)
 
 	bd := &EnvelopeBuilder{}
 	elp := bd.SetGasLimit(gaslimit).
 		SetGasPrice(gasprice).
-		SetAction(stake).Build()
+		SetAction(cr).Build()
 	h := elp.Hash()
-	require.Equal("f3e36f74dfe53c39c66a827244fc20f44b8f22db23e84776c4b1d2123a72c63a", hex.EncodeToString(h[:]))
+	require.Equal("f9d55837d0d13600012f25c3f9bd9207d1b38ea14f33918c63204fc6222f8e0e", hex.EncodeToString(h[:]))
 	// sign
 	selp, err := Sign(elp, senderKey)
 	require.NoError(err)
 	require.NotNil(selp)
 	ser, err := proto.Marshal(selp.Proto())
 	require.NoError(err)
-	require.Equal("0a1d080118c0843d22023130e20210080a10e807180122077061796c6f6164124104755ce6d8903f6b3793bddb4ea5d3589d637de2d209ae0ea930815c82db564ee8cc448886f639e8a0c7e94e99a5c1335b583c0bc76ef30dd6a1038ed9da8daf331a41e2e763aed5b1fd1a8601de0f0ae34eb05162e34b0389ae3418eedbf762f64959634a968313a6516dba3a97b34efba4753bbed3a33d409ecbd45ac75007cd8e9101", hex.EncodeToString(ser))
+	require.Equal("0acf01080118c0843d22023130fa02c1010a81010a29696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611229696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611a29696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a611202313018e80720012a29696f3178707136326177383575717a72636367397935686e727976386c64326e6b7079636333677a6132077061796c6f6164124104755ce6d8903f6b3793bddb4ea5d3589d637de2d209ae0ea930815c82db564ee8cc448886f639e8a0c7e94e99a5c1335b583c0bc76ef30dd6a1038ed9da8daf331a416290a11eecf59e84f75e3b0485e65d54b482c260c8ecc35cc7d690c02f0d319b377d3bfc63c3e0e009f88efe14c98dce68405f666dd6b634564f53525c09e65a00", hex.EncodeToString(ser))
 	hash := selp.Hash()
-	require.Equal("8816e8f784a1fce40b54d1cd172bb6976fd9552f1570c73d1d9fcdc5635424a9", hex.EncodeToString(hash[:]))
+	require.Equal("f662051a104f3be26125316bba8361d51035346502811550f4731d3c9132854c", hex.EncodeToString(hash[:]))
 	// verify signature
 	require.NoError(Verify(selp))
 }
