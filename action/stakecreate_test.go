1c2a0b7c422737a1fb2e983a98d8107adcdddff3
lzxm160 <lzxm160@sina.com>
2020-03-07 10:30:11 +0800

    x

--- action/stakecreate_test.go
+++ action/stakecreate_test.go
@@ -20,19 +20,6 @@ import (
 	"github.com/iotexproject/iotex-core/test/identityset"
 )
 
-var (
-	gaslimit   = uint64(1000000)
-	gasprice   = big.NewInt(10)
-	canAddress = "io1xpq62aw85uqzrccg9y5hnryv8ld2nkpycc3gza"
-	payload    = []byte("payload")
-	amount     = big.NewInt(10)
-	nonce      = uint64(0)
-	duration   = uint32(1000)
-	autoStake  = true
-	index      = uint64(10)
-	senderKey  = identityset.PrivateKey(27)
-)
-
 var stakeCreateTestParams = []struct {
 	SenderKey    crypto.PrivateKey
 	Nonce        uint64
