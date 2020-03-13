fd451286bcc732e27d33b87c30e8c3645d8dbc0b
lzxm160 <lzxm160@sina.com>
2020-03-13 15:35:49 +0800

    x

--- action/protocol/staking/handlers_test.go
+++ action/protocol/staking/handlers_test.go
@@ -295,7 +295,7 @@ func TestProtocol_HandleUnstake(t *testing.T) {
 
 		if test.errorCause == nil {
 			// test bucket index and bucket
-			bucketIndices, err := getCandBucketIndices(sm, candidate.Owner)
+			bucketIndices, err := getCandBucketIndices(sm, callerAddr)
 			require.NoError(err)
 			require.Equal(1, len(*bucketIndices))
 			bucketIndices, err = getVoterBucketIndices(sm, callerAddr)
