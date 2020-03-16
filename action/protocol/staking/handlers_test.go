b38d405926485ddfeac6b296280036f3261549b3
lzxm160 <lzxm160@sina.com>
2020-03-16 09:42:19 +0800

    x

--- action/protocol/staking/handlers_test.go
+++ action/protocol/staking/handlers_test.go
@@ -607,11 +607,11 @@ func TestProtocol_HandleWithdrawStake(t *testing.T) {
 		if test.errorCause == nil {
 			// test bucket index and bucket
 			bucketIndices, err := getCandBucketIndices(sm, candidate.Owner)
-			require.NoError(err)
-			require.Equal(2, len(*bucketIndices))
+			require.Error(err)
+			require.Equal(0, len(*bucketIndices))
 			bucketIndices, err = getVoterBucketIndices(sm, candidate.Owner)
-			require.NoError(err)
-			require.Equal(2, len(*bucketIndices))
+			require.Error(err)
+			require.Equal(0, len(*bucketIndices))
 			indices := *bucketIndices
 			bucket, err := getBucket(sm, indices[0])
 			require.NoError(err)
