589e8e9be79aa15f86beec18e23e59503f8b0e20
lzxm160 <lzxm160@sina.com>
2020-03-16 09:02:35 +0800

    x

--- action/protocol/staking/handlers.go
+++ action/protocol/staking/handlers.go
@@ -170,9 +170,9 @@ func (p *Protocol) handleWithdrawStake(ctx context.Context, act *action.Withdraw
 	if bucket.UnstakeStartTime.Unix() == 0 {
 		return nil, ErrNotUnstaked
 	}
-	fmt.Println(blkCtx.BlockTimeStamp)
-	fmt.Println(bucket.UnstakeStartTime)
-	fmt.Println(bucket.UnstakeStartTime.Add(p.config.WithdrawWaitingPeriod))
+	fmt.Println(blkCtx.BlockTimeStamp.UTC())
+	fmt.Println(bucket.UnstakeStartTime.UTC())
+	fmt.Println(bucket.UnstakeStartTime.Add(p.config.WithdrawWaitingPeriod).UTC())
 	if blkCtx.BlockTimeStamp.Before(bucket.UnstakeStartTime.Add(p.config.WithdrawWaitingPeriod)) {
 		return nil, errors.Wrap(ErrNotReadyWithdraw, fmt.Sprintf("stake is not ready to withdraw, current time %s, required time %s",
 			blkCtx.BlockTimeStamp, bucket.UnstakeStartTime.Add(p.config.WithdrawWaitingPeriod)))
