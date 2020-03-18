fe7336d88c51d62a440405ffa3b8d59463cccbb8
lzxm160 <lzxm160@sina.com>
2020-03-18 09:09:58 +0800

    x

--- action/protocol/staking/handlers_test.go
+++ action/protocol/staking/handlers_test.go
@@ -897,7 +897,7 @@ func TestProtocol_HandleChangeCandidate(t *testing.T) {
 		// fetchCaller state.ErrNotEnoughBalance
 		{
 			identityset.Address(1),
-			"999990000000000000000",
+			"9999990000000000000000",
 			10,
 			false,
 			1,
