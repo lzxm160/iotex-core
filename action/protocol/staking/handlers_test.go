2dfc584e9f1acba2a46c31b1b86b00d83512d1dd
lzxm160 <lzxm160@sina.com>
2020-03-18 12:47:30 +0800

    x

--- action/protocol/staking/handlers_test.go
+++ action/protocol/staking/handlers_test.go
@@ -1194,7 +1194,7 @@ func TestProtocol_HandleTransferStake(t *testing.T) {
 			require.NoError(err)
 			require.Equal(test.nonce, caller.Nonce)
 			total := big.NewInt(0)
-			require.Equal(unit.ConvertIotxToRau(test.initBalance), total.Add(total, caller.Balance).Add(total, actCost).Add(total, createCost))
+			require.Equal(unit.ConvertIotxToRau(test.initBalance), total.Add(total, caller.Balance).Add(total, actCost))
 		}
 	}
 }
