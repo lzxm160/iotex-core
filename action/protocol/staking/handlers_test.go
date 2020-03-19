25bbe9afdc6303724441d5c7a3496930a3dd7cdb
lzxm160 <lzxm160@sina.com>
2020-03-19 14:43:28 +0800

    x

--- action/protocol/staking/handlers_test.go
+++ action/protocol/staking/handlers_test.go
@@ -1469,10 +1469,10 @@ func TestProtocol_HandleRestake(t *testing.T) {
 			// test candidate
 			candidate, err = getCandidate(sm, candidate.Owner)
 			require.NoError(err)
-			require.Equal(uint64(2), candidate.Votes.String())
+			require.Equal("10380178401692392587", candidate.Votes.String())
 			candidate = p.inMemCandidates.GetByOwner(candidate.Owner)
 			require.NotNil(candidate)
-			require.Equal(uint64(2), candidate.Votes.Uint64())
+			require.Equal("10380178401692392587", candidate.Votes.Uint64())
 		}
 
 	}
