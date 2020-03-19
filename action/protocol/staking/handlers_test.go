0290c3724c249294b832cae08465fbd48664cebf
lzxm160 <lzxm160@sina.com>
2020-03-19 10:21:06 +0800

    x

--- action/protocol/staking/handlers_test.go
+++ action/protocol/staking/handlers_test.go
@@ -450,7 +450,7 @@ func TestProtocol_handleCandidateUpdate(t *testing.T) {
 			identityset.Address(31).String(),
 			identityset.Address(32).String(),
 			nil,
-			iotextypes.ReceiptStatus_Success,
+			iotextypes.ReceiptStatus_ErrCandidateNotExist,
 		},
 		{
 			1000,
