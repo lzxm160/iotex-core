22aa75b410f599c6bebd03e3f1e8c298b1f834b1
lzxm160 <lzxm160@sina.com>
2020-05-07 17:41:52 +0800

    x

--- tools/minicluster/minicluster.go
+++ tools/minicluster/minicluster.go
@@ -431,7 +431,7 @@ func newConfig(
 	cfg.Genesis.Blockchain.TimeBasedRotation = true
 	cfg.Genesis.Delegates = cfg.Genesis.Delegates[3 : numNodes+3]
 	cfg.Genesis.EnableGravityChainVoting = false
-	cfg.Genesis.PollMode = "lifeLong"
+	cfg.Genesis.PollMode = "native"
 	//scoreThreshold: "2000000000000000000000000"
 	//selfStakingThreshold: "1200000000000000000000000"
 	//stakingContractAddress: 0x87c9dbff0016af23f5b1ab9b8e072124ab729193
