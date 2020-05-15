911586227ff18164a047595703563a2d72298ec9
lzxm160 <lzxm160@sina.com>
2020-05-15 08:41:03 +0800

    x

--- ioctl/config/config.go
+++ ioctl/config/config.go
@@ -12,7 +12,8 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-
+	"gopkg.in/yaml.v2"
+	
 	"github.com/iotexproject/iotex-core/config"
 	"github.com/iotexproject/iotex-core/ioctl/output"
 	"github.com/iotexproject/iotex-core/pkg/log"
