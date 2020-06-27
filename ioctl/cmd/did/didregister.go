0d4adbabde4acbc2b719faac00d2e3d2144e1d59
lzxm160 <lzxm160@sina.com>
2020-06-27 11:53:21 +0800

    x

--- ioctl/cmd/did/didregister.go
+++ ioctl/cmd/did/didregister.go
@@ -7,17 +7,12 @@
 package did
 
 import (
-	"crypto"
 	"encoding/hex"
 	"errors"
-	"fmt"
 	"math/big"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
-
-	"github.com/iotexproject/iotex-core/ioctl/cmd/account"
-
 	"github.com/spf13/cobra"
 
 	"github.com/iotexproject/iotex-core/ioctl/cmd/action"
@@ -27,13 +22,8 @@ import (
 )
 
 const (
-	deregisterDIDFunc          = "3d039cb3"
-	getHashFunc                = "b00140aa"
-	getURIFunc                 = "8626dea9"
-	registerDIDFunc            = "627c625a"
-	updateDIDFunc              = "72e0b98b"
-	AddressBasedDIDManagerABI  = `[    {        "inputs": [            {                "internalType": "bytes",                "name": "_prefix",                "type": "bytes"            },            {                "internalType": "address",                "name": "_dbAddr",                "type": "address"            }        ],        "payable": false,        "stateMutability": "nonpayable",        "type": "constructor"    },                {        "constant": false,        "inputs": [],        "name": "deregisterDID",        "outputs": [],        "payable": false,        "stateMutability": "nonpayable",        "type": "function"    },    {        "constant": true,        "inputs": [            {                "internalType": "bytes",                "name": "did",                "type": "bytes"            }        ],        "name": "getHash",        "outputs": [            {                "internalType": "bytes32",                "name": "",                "type": "bytes32"            }        ],        "payable": false,        "stateMutability": "view",        "type": "function"    },       {        "constant": true,        "inputs": [            {                "internalType": "bytes",                "name": "did",                "type": "bytes"            }        ],        "name": "getURI",        "outputs": [            {                "internalType": "bytes",                "name": "",                "type": "bytes"            }        ],        "payable": false,        "stateMutability": "view",        "type": "function"    },        {        "constant": false,        "inputs": [            {                "internalType": "bytes32",                "name": "h",                "type": "bytes32"            },            {                "internalType": "bytes",                "name": "uri",                "type": "bytes"            }        ],        "name": "registerDID",        "outputs": [],        "payable": false,        "stateMutability": "nonpayable",        "type": "function"    },    {        "constant": false,        "inputs": [            {                "internalType": "bytes32",                "name": "h",                "type": "bytes32"            },            {                "internalType": "bytes",                "name": "uri",                "type": "bytes"            }        ],        "name": "updateDID",        "outputs": [],        "payable": false,        "stateMutability": "nonpayable",        "type": "function"    }]`
-	AddressBasedDIDManagerABI2 = "[{\"inputs\":[{\"internalType\":\"bytes\",\"name\":\"_prefix\",\"type\":\"bytes\"},{\"internalType\":\"address\",\"name\":\"_dbAddr\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"string\",\"name\":\"did\",\"type\":\"string\"},{\"indexed\":false,\"internalType\":\"bytes32\",\"name\":\"hash\",\"type\":\"bytes32\"},{\"indexed\":false,\"internalType\":\"string\",\"name\":\"uri\",\"type\":\"string\"}],\"name\":\"DIDCreated\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"string\",\"name\":\"did\",\"type\":\"string\"}],\"name\":\"DIDDeleted\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"string\",\"name\":\"did\",\"type\":\"string\"},{\"indexed\":false,\"internalType\":\"bytes32\",\"name\":\"hash\",\"type\":\"bytes32\"},{\"indexed\":false,\"internalType\":\"string\",\"name\":\"uri\",\"type\":\"string\"}],\"name\":\"DIDUpdated\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"previousOwner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"OwnershipTransferred\",\"type\":\"event\"},{\"constant\":true,\"inputs\":[],\"name\":\"db\",\"outputs\":[{\"internalType\":\"contractDIDStorage\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"bytes\",\"name\":\"did\",\"type\":\"bytes\"}],\"name\":\"decodeInternalKey\",\"outputs\":[{\"internalType\":\"bytes20\",\"name\":\"\",\"type\":\"bytes20\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"deregisterDID\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"bytes\",\"name\":\"did\",\"type\":\"bytes\"}],\"name\":\"getHash\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"bytes\",\"name\":\"did\",\"type\":\"bytes\"}],\"name\":\"getOwner\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"bytes\",\"name\":\"did\",\"type\":\"bytes\"}],\"name\":\"getURI\",\"outputs\":[{\"internalType\":\"bytes\",\"name\":\"\",\"type\":\"bytes\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"h\",\"type\":\"bytes32\"},{\"internalType\":\"bytes\",\"name\":\"uri\",\"type\":\"bytes\"}],\"name\":\"registerDID\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"transferDBOwnership\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"transferOwnership\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"h\",\"type\":\"bytes32\"},{\"internalType\":\"bytes\",\"name\":\"uri\",\"type\":\"bytes\"}],\"name\":\"updateDID\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+	registerDIDName           = "registerDID"
+	AddressBasedDIDManagerABI = `[{"inputs": [{"internalType": "bytes","name": "_prefix","type": "bytes"},{"internalType": "address","name": "_dbAddr","type": "address"}],"payable": false,"stateMutability": "nonpayable","type": "constructor"},{"constant": false,"inputs": [],"name": "deregisterDID","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [{"internalType": "bytes","name": "did","type": "bytes"}],"name": "getHash","outputs": [{"internalType": "bytes32","name": "","type": "bytes32"}],"payable": false,"stateMutability": "view","type": "function"},   {"constant": true,"inputs": [{"internalType": "bytes","name": "did","type": "bytes"}],"name": "getURI","outputs": [{"internalType": "bytes","name": "","type": "bytes"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": false,"inputs": [{"internalType": "bytes32","name": "h","type": "bytes32"},{"internalType": "bytes","name": "uri","type": "bytes"}],"name": "registerDID","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": false,"inputs": [{"internalType": "bytes32","name": "h","type": "bytes32"},{"internalType": "bytes","name": "uri","type": "bytes"}],"name": "updateDID","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"}]`
 )
 
 // Multi-language support
@@ -89,22 +79,23 @@ func encode(didHash, uri string) (ret []byte, err error) {
 	if err != nil {
 		return
 	}
-	_, exist := abi.Methods["registerDID"]
+	_, exist := abi.Methods[registerDIDName]
 	if !exist {
 		return nil, errors.New("method is not found")
 	}
-	return abi.Pack("registerDID", hashArray, []byte(uri))
+	return abi.Pack(registerDIDName, hashArray, []byte(uri))
 }
 
-func getPrivate() (crypto.PrivateKey, error) {
-	addr, err := action.Signer()
-	if err != nil {
-		return nil, output.NewError(output.InputError, "failed to get signer addr", err)
-	}
-	fmt.Printf("Enter password #%s:\n", addr)
-	password, err := util.ReadSecretFromStdin()
-	if err != nil {
-		return nil, output.NewError(output.InputError, "failed to get password", err)
-	}
-	return account.LocalAccountToPrivateKey(addr, password)
-}
+//
+//func getPrivate() (crypto.PrivateKey, error) {
+//	addr, err := action.Signer()
+//	if err != nil {
+//		return nil, output.NewError(output.InputError, "failed to get signer addr", err)
+//	}
+//	fmt.Printf("Enter password #%s:\n", addr)
+//	password, err := util.ReadSecretFromStdin()
+//	if err != nil {
+//		return nil, output.NewError(output.InputError, "failed to get password", err)
+//	}
+//	return account.LocalAccountToPrivateKey(addr, password)
+//}
