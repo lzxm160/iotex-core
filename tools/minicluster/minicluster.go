ad3f54300647996090166648518ebfaedf022a62
lzxm160 <lzxm160@sina.com>
2020-07-23 15:48:30 +0800

    add bucket candidate indexer

--- tools/minicluster/minicluster.go
+++ tools/minicluster/minicluster.go
@@ -405,7 +405,7 @@ func newConfig(
 
 	cfg.Plugins[config.GatewayPlugin] = true
 	cfg.Chain.EnableAsyncIndexWrite = false
-
+	cfg.Chain.EnableStakingIndexer = true
 	cfg.System.HTTPAdminPort = HTTPAdminPort
 	cfg.Network.Port = networkPort
 	cfg.Network.BootstrapNodes = []string{"/ip4/127.0.0.1/tcp/4689/ipfs/12D3KooWJwW6pUpTkxPTMv84RPLPMQVEAjZ6fvJuX4oZrvW5DAGQ"}
