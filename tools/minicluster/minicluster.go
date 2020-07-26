0e6e0f89e849ec5122e278b92ce191e4e7c2a72a
lzxm160 <lzxm160@sina.com>
2020-07-27 07:17:44 +0800

    x

--- tools/minicluster/minicluster.go
+++ tools/minicluster/minicluster.go
@@ -432,7 +432,7 @@ func newConfig(
 	cfg.Genesis.Delegates = cfg.Genesis.Delegates[3 : numNodes+3]
 	cfg.Genesis.EnableGravityChainVoting = false
 	cfg.Genesis.PollMode = "lifeLong"
-	cfg.Genesis.FairbankBlockHeight = 10
+	cfg.Genesis.FairbankBlockHeight = 5
 	cfg.Genesis.GreenlandBlockHeight = 20
 	cfg.Genesis.InitBalanceMap = make(map[string]string)
 	for _, a := range addr {
