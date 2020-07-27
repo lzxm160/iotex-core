5bd99916d53e4175ce781ac358f278c583a794b6
lzxm160 <lzxm160@sina.com>
2020-07-27 08:19:52 +0800

    x

--- tools/minicluster/minicluster.go
+++ tools/minicluster/minicluster.go
@@ -263,6 +263,7 @@ func main() {
 		expectedBalancesMap := util.GetAllBalanceMap(client, chainAddrs)
 		pendingActionMap := new(sync.Map)
 		log.L().Info("Start action injections.")
+		time.Sleep(time.Second * 50)
 		injectCandidates(chainAddrs)
 		time.Sleep(time.Second * 10)
 		injectBuckets()
@@ -431,8 +432,8 @@ func newConfig(
 	cfg.Genesis.Delegates = cfg.Genesis.Delegates[3 : numNodes+3]
 	cfg.Genesis.EnableGravityChainVoting = false
 	cfg.Genesis.PollMode = "lifeLong"
-	cfg.Genesis.FairbankBlockHeight = 1
-	cfg.Genesis.GreenlandBlockHeight = 20
+	cfg.Genesis.FairbankBlockHeight = 5
+	cfg.Genesis.GreenlandBlockHeight = 6
 	cfg.Genesis.InitBalanceMap = make(map[string]string)
 	for _, a := range addr {
 		cfg.Genesis.InitBalanceMap[a.EncodedAddr] = "1000000000000000000000000000000000000000000"
