9211e3e8896fd057cce1f5886244cd3e9a5daf9e
lzxm160 <lzxm160@sina.com>
2020-09-13 21:57:02 +0800

    x

--- action/protocol/staking/protocol.go
+++ action/protocol/staking/protocol.go
@@ -217,7 +217,7 @@ func (p *Protocol) CreatePreStates(ctx context.Context, sm protocol.StateManager
 	bcCtx := protocol.MustGetBlockchainCtx(ctx)
 	blkCtx := protocol.MustGetBlockCtx(ctx)
 	hu := config.NewHeightUpgrade(&bcCtx.Genesis)
-	fmt.Println("CreatePreStates///////", blkCtx.BlockHeight, config.Fairbank, config.Greenland)
+	fmt.Println("CreatePreStates///////", blkCtx.BlockHeight, p.hu.FairbankBlockHeight(), p.hu.GreenlandBlockHeight())
 	if p.enableArchiveMode && p.hu.IsPost(config.Fairbank, blkCtx.BlockHeight) && p.hu.IsPre(config.Greenland, blkCtx.BlockHeight) {
 		p.saveStakingAddressHistory(blkCtx.BlockHeight, sm)
 	}
