a66bc826cd0eb93dbe54fac6b30c809947b357a4
lzxm160 <lzxm160@sina.com>
2020-10-06 12:28:00 +0800

    x

--- action/protocol/staking/protocol.go
+++ action/protocol/staking/protocol.go
@@ -479,7 +479,7 @@ func (p *Protocol) ReadState(ctx context.Context, sr protocol.StateReader, metho
 		if p.archiveMode && inputHeight < p.hu.GreenlandBlockHeight() {
 			if p.candBucketsIndexer != nil {
 				resp, height, err = p.candBucketsIndexer.GetStakingBalance(inputHeight)
-				log.L().Info("477.....", zap.String("resp", resp.String()), zap.Uint64("height", height), zap.Error(err), zap.Uint64("FairbankBlockHeight", p.hu.FairbankBlockHeight()))
+				log.L().Info("477.....", zap.String("resp", resp.String()), zap.Uint64("height", height), zap.Uint64("FairbankBlockHeight", p.hu.FairbankBlockHeight()))
 			} else {
 				resp, height, err = &iotextypes.AccountMeta{
 					Address: address.StakingBucketPoolAddr,
